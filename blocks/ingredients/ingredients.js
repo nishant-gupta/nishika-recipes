e737506d07b21aa7148c2cd0f57c15d9015b4d6d
nishant-gupta <gnishant@gmail.com>
2025-06-06 17:27:28 +0530

    index

--- blocks/ingredients/ingredients.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-  // do nothing
-  block.classList.add('ingredients');
-  block.children.forEach((child) => {
-    child.classList.add('ingredient-item');
-  });
-}
